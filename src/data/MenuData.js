ef4b790261d585ca990cba672014de07931f79b1
m4niga <agustinguerr4@gmail.com>
2020-12-21 06:33:33 -0300

    Navbar

---
+++ src/data/MenuData.js
@@ -0,0 +1,18 @@
+export const menuData = [
+  {
+    title: 'Inicio', 
+    to: '/' 
+  }, 
+  {
+    title: 'Nosotros', 
+    to: '/' 
+  }, 
+  {
+    title: 'Servicios', 
+    to: '/' 
+  },
+  {
+    title: 'Contacto', 
+    to: '/' 
+  }
+]
