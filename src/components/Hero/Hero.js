709dbd6fb22599192e5fdadddfac46eac59e660c
m4niga <agustinguerr4@gmail.com>
2020-12-21 08:37:02 -0300

    Add Functionality to Slider

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled, { css } from 'styled-components/macro';
 import { Button } from '../Navbar/Button/Button';
 import { IoMdArrowRoundForward } from 'react-icons/io';
@@ -88,7 +88,9 @@ p{
 }
 
 `;
-const Arrow = styled(IoMdArrowRoundForward)``;
+const Arrow = styled(IoMdArrowRoundForward)`
+  margin-left: 0.5rem;
+`;
 
 const SliderButtons = styled.div`
   position:absolute;
@@ -125,6 +127,45 @@ ${ArrowButtons}
 `;
 
 const Hero = ({ slides }) => {
+
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+  const timeout = useRef(null);
+
+  useEffect(() => {
+    const nextSlide = () => {
+      setCurrent(current => (current === length -1 ? 0 : current +1))
+    }
+
+    timeout.current = setTimeout(nextSlide,5000)
+
+    return function(){
+      if(timeout.current){
+        clearTimeout(timeout.current)
+      }
+    }
+  }, [current,length])
+
+  const nextSlide = () => {
+    if(timeout.current){
+      clearTimeout(timeout.current)
+    }
+
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  const prevSlide = () => {
+    if(timeout.current){
+      clearTimeout(timeout.current)
+    }
+    
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  if(!Array.isArray(slides) || slides.length <= 0){
+    return null
+  }
+
   return (
     <HeroSection>
       <HeroWrapper>
@@ -132,29 +173,34 @@ const Hero = ({ slides }) => {
           slides.map((slide, index) => {
             return (
               <HeroSlide key={index}>
-                <HeroSlider>
-                  <HeroImage 
-                  src={slide.image}/>
-                  <HeroContent>
-                    <h1>{slide.title}</h1>
-                    <p>{slide.subtitle}</p>
-                    <Button 
-                      to={slide.to} 
-                      primary='true'
-                      css={`max-width:160px`} 
-                      >
-                      {slide.label}
-                      <Arrow />
-                    </Button>
-                  </HeroContent>
-                </HeroSlider>
+                {
+                  index === current && (
+
+                    <HeroSlider>
+                      <HeroImage
+                        src={slide.image} />
+                      <HeroContent>
+                        <h1>{slide.title}</h1>
+                        <p>{slide.subtitle}</p>
+                        <Button
+                          to={slide.to}
+                          primary='true'
+                          css={`max-width:160px`}
+                        >
+                          {slide.label}
+                          <Arrow />
+                        </Button>
+                      </HeroContent>
+                    </HeroSlider>
+                  )
+                }
               </HeroSlide>
             )
           })
         }
         <SliderButtons>
-          <PrevArrow/>
-          <NextArrow />
+          <PrevArrow onClick={prevSlide} />
+          <NextArrow onClick={nextSlide} />
         </SliderButtons>
       </HeroWrapper>
 
